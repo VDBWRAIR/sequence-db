d0e79304c9f13af7ec8c9086d1ccc74396418708
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2016-04-26 15:17:30 -0400

    most features working

--- src/Form.purs
+++ src/Form.purs
@@ -1,14 +1,14 @@
 module App.Form where
 import Global (encodeURIComponent)
 import App.Routes (Route)
-import Prelude (($), map, (<>), show, const, (<<<), (&&), (<=), (>=), (<$>), (==), Eq)
-import Pux.Html (Html, text, form, button, input, span, ul, div, a)
-import Pux.Html.Attributes (type_, value, name, download, href)
-import Pux.Html.Events (FormEvent, onChange, onSubmit, onClick)
+import Prelude --(($), map, (<>), show, const, (<<<), (&&), (<=), (>=), (<$>), (==), Eq, not)
+import Pux.Html (Html, text, form, button, input, span, ul, div, label, a, br, p, select, option)
+import Pux.Html.Attributes (type_, value, name, download, href, checked, disabled)
+import Pux.Html.Events (FormEvent, onChange, onSubmit, onClick, SelectionEvent, onSelect)
 import Unsafe.Coerce (unsafeCoerce)
 import Data.Maybe (Maybe(Nothing, Just), fromMaybe)
 import App.Seq as Seq
-import Data.Array (filter, nubBy)
+import Data.Array (filter, nubBy, length)
 import Data.Foldable (intercalate)
 type Year = Int
 
@@ -21,7 +21,11 @@ type State = {    name      :: Maybe String
                  , host     :: Maybe Seq.Host
                  , serotype :: Maybe Seq.Serotype
                  , result   :: Array Seq.State
+                 , random   :: Boolean
+                 , sampleSize :: Maybe Int
+                 , format   :: Seq.Format
               }
+type Acc = String
 data Action =
    NameChange     FormEvent
  | MinYearChange  FormEvent
@@ -30,18 +34,22 @@ data Action =
  | HostChange     FormEvent
  | SerotypeChange FormEvent
  | SegmentChange  FormEvent
+ | SampleSizeChange  FormEvent
+ | FormatChange SelectionEvent
  | RunQuery
- | ResetResults
+ | DelteChecked
+ | ToggleRandom
  | PageView Route
- | Child Seq.Action
-
+ | Child Seq.Acc Seq.Action
   
 init :: State
 init = { name: Nothing, country: Nothing
        , host: Nothing, serotype: Nothing
        , segment: Nothing
        , minYear : 0, maxYear : 3000
-       , acc : Nothing, result : [] }
+       , acc : Nothing, result : []
+       , random : false, sampleSize : Nothing
+       , format : Seq.Fasta}
 -- In order to give Seq.State an Eq instance, it must be wrapped in NewType
 update :: Action -> State -> State
 update (RunQuery) state = state { result = nubBy Seq.stateEq $ state.result <> (query state) }
@@ -53,27 +61,53 @@ update (MaxYearChange ev) state = state { maxYear = (unsafeCoerce ev.target.valu
 update (HostChange ev)    state = state { host = Just (unsafeCoerce ev.target.value :: Seq.Host) }
 update (SerotypeChange ev)    state = state { serotype = Just (unsafeCoerce ev.target.value :: Seq.Serotype) }
 update (SegmentChange ev)    state = state { segment = Just (unsafeCoerce ev.target.value :: Seq.Segment) }
-update ResetResults     state = state { result = [] }
+update (SampleSizeChange ev)    state = state { sampleSize = Just (unsafeCoerce ev.target.value :: Int ) }
+update DelteChecked     state = state { result = (filter (not <<< _.checked) state.result )}
+update ToggleRandom     state = state { random = not state.random }
+update (Child acc Seq.ToggleCheck) state = state { result = map f state.result }
+  where f x = if (x.acc == acc) then (Seq.update Seq.ToggleCheck x) else x -- (x {checked = not x.checked} ) else x
+update (FormatChange ev)    state = state { format = (unsafeCoerce ev.target.value :: Seq.Format ) }
 
 view :: State -> Html Action
-view state =
-  div []
+view state = div []
   [form
   [ name "Search"
   , onSubmit (const RunQuery)
     ]
-  [ input [ name "Name:", type_ "text", value $ fromMaybe "" state.name,    onChange NameChange ] []
-  , input [ name "Country:", type_ "text", value $ fromMaybe "" state.country, onChange CountryChange ] []
-  , input [ name "Host Species:", type_ "text", value $ fromMaybe "" $ show <$> state.host, onChange HostChange ] []
-  , input [ name "Segmeent (optional):", type_ "text", value $ fromMaybe "" $ show <$> state.segment, onChange SegmentChange ] []
-  , input [ name "Serotype:", type_ "text", value $ fromMaybe "" $ show <$> state.serotype, onChange SerotypeChange ] []
-  , input [ name "Minimum Year", type_ "text", value $ show state.minYear, onChange MinYearChange ] []
-  , input [ name "Maximum Year", type_ "text", value $ show state.maxYear, onChange MaxYearChange ] []
+  [ label [] [ text "Name:"], input [type_ "text", value $ fromMaybe "" state.name,    onChange NameChange ] [] 
+  , label [] [ text "Country:"], input [type_ "text", value $ fromMaybe "" state.country, onChange CountryChange ] [] 
+  , label [] [ text "Host Species:"], input [type_ "text", value $ fromMaybe "" $ show <$> state.host, onChange HostChange ] [] , br [] []
+  , label [] [ text "Segmeent (optional):"], input [type_ "text", value $ fromMaybe "" $ show <$> state.segment, onChange SegmentChange ] []
+  , label [] [ text "Serotype:"], input [type_ "text", value $ fromMaybe "" $ show <$> state.serotype, onChange SerotypeChange ] [], br [] []
+  , label [] [ text "Minimum Year"], input [type_ "text", value $ show state.minYear, onChange MinYearChange ] []
+  , label [] [ text "Maximum Year"], input [type_ "text", value $ show state.maxYear, onChange MaxYearChange ] []
   , button [ type_ "submit" ] [ text "Search" ]
-  , ul [] $  map ((map Child) <<< Seq.view) state.result
+  --, ul [] $  map ((map Child) <<< Seq.view) state.result
+  , ul [] $  map (\s -> map (Child s.acc) $ Seq.view s) state.result
     ]
-  , button [ onClick (const ResetResults)] [ text "Reset" ]
-  , a [href ("data:text/plain;charset=utf-8," <> (encodeURIComponent $ toCSV state.result)) ] [text "Download"]]
+  , button [ onClick (const DelteChecked)] [ text "Delete" ],
+    p [] [text ("Results: " ++ (show $ length state.result))], br [] [] 
+  , span [] [label [] [text "Random subset"], br [] []
+           , input [type_ "checkbox", checked state.random, value "random" , onClick (const ToggleRandom)] []
+           , label [] [text "Sample Size"]
+           , input [disabled $ not state.random, type_ "text", value $ fromMaybe "" $ show <$> state.sampleSize, onChange SampleSizeChange] []
+           , br [] 
+    []
+  , a [href ("data:text/plain;charset=utf-8," <> (encodeURIComponent $ toFormat state.format state.result)) ] [text "Download"]
+  , label [] [text "     Format"]]
+  , select [onSelect FormatChange] [option [value $ show Seq.Fasta] [text $ show Seq.Fasta],
+               option [value $ show Seq.CSV] [text $ show Seq.CSV]]]
+
+toFormat :: Seq.Format ->  Array Seq.State  -> String
+toFormat fmt xs = case fmt of
+  Seq.Fasta -> intercalate "\n" $ map toFasta xs
+  Seq.CSV   -> header <> "\n" <> (intercalate "\n" $ map toRow xs)
+  where
+    toFasta x = x.name <> "_" <> x.acc <> "\n" <> x.sequence
+    header = "name,acc,year,segment"
+    toRow x = intercalate "," [x.name, x.acc, (show x.year), (show x.segment)]
+--toFormat Seq.Fasta xs = intercalate "\n" $ map toFasta xs
+--toFormat Seq.CSV xs = toCSV xs
 
 toCSV :: Array Seq.State -> String
 toCSV xs = header <> "\n" <> (intercalate "\n" $ map toRow xs)
@@ -92,7 +126,7 @@ query q = filter match seqs
     (==?) :: forall a. (Eq a) => Maybe a -> a -> Boolean
     (==?) a b = fromMaybe true ((== b) <$> a)
 
-seqs  = [example]
+seqs  = [example, example2, example3]
 
 example :: Seq.State
 example =  {
@@ -104,4 +138,29 @@ example =  {
      , serotype : Seq.DENV1
      , sequence : "ACTG"
      , segment  : Nothing
+     , checked : false
+       }
+
+example2 :: Seq.State
+example2 =  {
+       name     : "NameToo"
+     , acc      : "AccToo"
+     , year     : 2012
+     , country  : "Jamaica"
+     , host     : Seq.Human
+     , serotype : Seq.DENV2
+     , sequence : "GGGGG"
+     , segment  : Nothing
+     , checked : false
+       }
+example3 =  {
+       name     : "Influenza99"
+     , acc      : "Acc"
+     , year     : 1999
+     , country  : "USA"
+     , host     : Seq.Human
+     , serotype : Seq.HN1
+     , sequence : "GGGGG"
+     , segment  : Just Seq.PB1
+     , checked : false
        }
--- src/Seq.purs
+++ src/Seq.purs
@@ -1,8 +1,9 @@
 module App.Seq where
 import App.Routes (Route(Home, NotFound))
-import Pux.Html (Html, div, p, text, table, tr, td)
-import Pux.Html.Attributes (className)
-import Prelude (id, const, ($), show, (<>), (<$>), Eq, Show, (==), (&&))
+import Pux.Html (Html, div, p, text, table, tr, td, input)
+import Pux.Html.Attributes (className, checked, value, type_)
+import Prelude (id, const, ($), show, (<>), (<$>), Eq, Show, (==), (&&), not)
+import Pux.Html.Events (onClick)
 import Data.Generic
 import Data.Maybe (Maybe, fromMaybe)
 
@@ -13,11 +14,18 @@ instance showHost :: Show Host where
 instance eqHost :: Eq Host where
     eq = gEq
     
-data Serotype = DENV1 | DENV2 | DENV3 | DENV4
+data Serotype = DENV1 | DENV2 | DENV3 | DENV4 | HN1
 derive instance genericSerotype :: Generic Serotype 
 instance showSerotype :: Show Serotype where
     show = gShow 
 instance eqSerotype :: Eq Serotype where
+    eq = gEq
+    
+data Format = Fasta | CSV
+derive instance genericFormat :: Generic Format
+instance showFormat :: Show Format where
+    show = gShow 
+instance eqFormat :: Eq Format where
     eq = gEq
 
 data Segment = PB1 | PB2 -- ... etc. 
@@ -26,6 +34,9 @@ instance showSegment :: Show Segment where
   show PB2 = "PB2"
 instance eqSegment :: Eq Segment where
   eq x y = (show x) == (show y) 
+
+
+
 
 type Year = Int 
 type State = {
@@ -37,6 +48,7 @@ type State = {
      , serotype :: Serotype
      , sequence :: String
      , segment  :: Maybe Segment
+     , checked  :: Boolean
        }
 stateEq :: State -> State -> Boolean
 stateEq x y = x.name == y.name &&
@@ -46,21 +58,25 @@ stateEq x y = x.name == y.name &&
               x.host  == y.host  && 
               x.serotype  == y.serotype  && 
               x.segment  == y.segment
-              
-data Action = PageView Route
+type Acc = String              
+data Action = PageView Route | ToggleCheck
 --TODO: add action to delete/check (check for deletion / download)
 view :: State -> Html Action
 view state = table []
-            [tr [] [ td [className "name"]    [ text $ "Name:  " <> state.name ]
-            , td [className "acc"]     [ text $ "Accession:  " <> state.acc ]
-            , td [className "year"]    [ text $ "Year:  " <> show state.year ]
-            , td [className "country"] [ text $ "Country:  " <> state.country ] ]
+            [tr []
+              [ td  [] 
+                   [input [type_ "checkbox", checked state.checked, value "selected" , onClick $ const ToggleCheck] [] ]
+              , td [className "name"]    [ text $ "Name:  " <> state.name ]
+              , td [className "acc"]     [ text $ "Accession:  " <> state.acc ]
+              , td [className "year"]    [ text $ "Year:  " <> show state.year ]
+              , td [className "country"] [ text $ "Country:  " <> state.country ] ]
           , tr []   [ td []  [ text $ "Host:  " <> show state.host ] 
                  ,  td  [] [ text $ "Serotype:  " <> show state.serotype ] 
                  ,  td  [] [ text $ "Segment:  "  <> (fromMaybe "n/a" $ show <$> state.segment)] ]]
 
 update :: Action -> State -> State
-update = const id
+update ToggleCheck state = state { checked = not state.checked }
+update _ state = state 
 
 init :: State -> State
 init = id
