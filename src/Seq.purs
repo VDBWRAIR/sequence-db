da51c27b4bf66f182efd6ba2c1439198230fd3a1
michaelpanciera <michael.panciera.work@gmail.com>
2016-04-30 21:59:19 -0400

    stuff for CSV parsing

--- src/Seq.purs
+++ src/Seq.purs
@@ -7,20 +7,31 @@ import Data.String (split)
 import App.Routes (Route(Home, NotFound))
 import Pux.Html (Html, div, p, text, table, tr, td, input)
 import Pux.Html.Attributes (className, checked, value, type_)
-import Prelude (id, const, ($), show, (<>), (<$>), Eq, (==), (&&), not, (<<<), map, Show)
+import Prelude --(id, const, ($), show, (<>), (<$>), Eq, (==), (&&), not, (<<<), map, Show)
 import Pux.Html.Events (onClick)
 import Data.Foldable (Foldable)
 import Data.Generic
-import Data.Maybe (Maybe, fromMaybe)
+import Data.Maybe (Maybe, fromMaybe) 
+--import Text.Parsing.CSV (defaultParsers, makeParsers)
+--import Text.Parsing.Parser (runParser)
+import Data.Array.Unsafe (unsafeIndex)
+import Data.Array as A
+import Data.String as S
+import Data.Maybe
+import Data.Int as Int 
 --import Data.Eulalie.Parser as P
 --import Data.Eulalie.String as S
 
 
-undot :: String -> String    
-undot s = last $ split "." s
-
 data Format = Fasta | CSV
 readFormat = makeRead [Fasta, CSV]
+
+startsWith :: String -> String -> Boolean
+startsWith sub s = S.take (S.length sub) s == sub
+
+autoComplete :: (State -> String) -> Array State -> String -> Array String
+autoComplete attr recs sub = A.filter (startsWith sub) $ map attr recs
+
 derive instance genericFormat :: Generic Format
 instance showFormat :: Show Format where
   show = undot <<< gShow
@@ -36,6 +47,10 @@ instance showHost :: Show Host where
     show = undot <<< gShow 
 instance eqHost :: Eq Host where
     eq = gEq
+
+
+undot :: String -> String    
+undot s = last $ S.split "." s
 
 readSerotype = makeRead serotypes
 serotypes = [DENV1 , DENV2 , DENV3 , DENV4 , HN1 , H1N1 , H5N1 , H3N2 , H7N9]
@@ -75,7 +90,44 @@ type State = {
      , segment  :: Maybe Segment
      , genotype :: Maybe Genotype
      , checked  :: Boolean
-       }
+     }
+             
+--columns = map fst [("name", id') ,  ("acc", id') ,  ("country", id') ,  ("year", Int.fromString) ,  ("host", readHost) ,  ("seortype", readSegment) ,  ("segment", maybe' readSegment) ,  ("genotype", maybe' readGenotype)]
+
+-- TODO: include Dates
+columns :: Array String 
+columns = ["name", "acc", "country", "year", "host", "serotype", "segment", "genotype"]
+--    applyRow row = zipWith ($) funcs row
+--    funcs = map snd $ A.sortBy headerOrder columns
+readCSV :: String -> String -> Maybe (Array State)
+readCSV sep s = process <$> (A.head lines') <*> (A.tail lines')
+  where
+    lines' = map (S.split sep) $ lines s
+    lines  = S.split "\n"
+        
+process :: Array String -> Array (Array String) -> Array State
+process header rows = map process' rows
+  where
+    process' :: Array String -> State
+    process' row = {
+      name : (row `at` 0)
+    , acc  : (row `at` 1)
+    , country : (row `at` 2)
+    , year : fromMaybe 0 $ Int.fromString (row `at` 3)
+    , host : fromMaybe Human $ readHost (row `at` 3)
+    , serotype : fromMaybe DENV4 $ readSerotype (row `at` 4)
+    , segment : readSegment (row `at` 5)
+    , genotype : readGenotype (row `at` 6)
+    , checked : false
+    , sequence : "DummySeq"
+    }
+      where at = unsafeIndex
+    order row = A.sortBy headerOrder row
+    headerOrder x y  = compare (fromMaybe 9999 (A.elemIndex  x header)) (fromMaybe 9999 (A.elemIndex y header))
+    
+maybe' _ "" = Nothing
+maybe' f  x = Just $ f x
+
 stateEq :: State -> State -> Boolean
 stateEq x y = x.name == y.name &&
               x.acc  == y.acc  && 
@@ -85,6 +137,7 @@ stateEq x y = x.name == y.name &&
               x.serotype  == y.serotype  && 
               x.segment  == y.segment &&
               x.genotype == x.genotype
+              
 type Acc = String              
 data Action = PageView Route | ToggleCheck
 --TODO: add action to delete/check (check for deletion / download)
