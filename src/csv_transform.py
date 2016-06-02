8ee4e8964f530143efd13a3f0cd082a8fc82e6e3
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2016-06-02 11:56:55 -0400

    generalize csv_transform.py

--- src/csv_transform.py
+++ src/csv_transform.py
@@ -6,19 +6,21 @@ import csv
 from dateutil.parser import parse
 from fn import F, _
 import sys
+import os 
 log = sys.stderr.write
+
 def find(pred, seq):
     res = filter(pred, seq)
     return None if not res else res[0]
 
 segments = ["HA", "MP", "NA", "NP", "NS", "PA", "PB1", "PB2"]
-SEGMENT_DIR = "10454"
+#SEGMENT_DIR = "10454"
 #seg_seqs = map(seq_for_segment, segments)
 #return map(lambda seg,seq: assoc(new_d, seg, seq), segments, seg_seqs)
-def run(fp):
+def run(fp, segment_dir):
     rows = csv.DictReader(open(fp))
     rows = list(rows)[:10]
-    results = list(mapcat(handle_row, rows))
+    results = list(mapcat(F(handle_row, segment_dir), rows))
     head = results[0]
     writer = csv.DictWriter(sys.stdout, fieldnames=head.keys())
     writer.writeheader()
@@ -26,14 +28,15 @@ def run(fp):
         writer.writerow(result)
     
 
-def handle_row(row):
-    return map(F(row_for_segment, row), segments)
+def handle_row(segment_dir, row):
+    return map(F(row_for_segment, segment_dir, row), segments)
 
-def row_for_segment(old_d, seg): # (str) -> str
+def row_for_segment(segment_dir, old_d, seg): # (str) -> str
    try:
-      fa = glob("/home/michaelpanciera/sequence-db/{}/*{}*.fasta".format(SEGMENT_DIR, seg))[0]
+      dir = os.path.join(segment_dir, "*{}*.fasta".format(seg))
+      fa = glob(dir)[0]
    except Exception as e:
-       log(str(e))
+       log(str(e)+"\n")
        log("fasta file for segment %s not found\n" % seg)
        sys.exit(1)
    seqs = SeqIO.parse(fa, "fasta")
@@ -42,9 +45,13 @@ def row_for_segment(old_d, seg): # (str) -> str
    seq  = find(_.id == old_d['name'][1:], seqs)
    useless = map("segment{}".format, segments) + map("acc{}".format, segments)
    d = dissoc(old_d, *useless)
-   date = parse(old_d['date'])
-   date_info = dict(month=date.month, day=date.day, year=date.year)
-   if date == parse(""):
+   # in some version parse on the empty string returns today's date; in another version it throws a value error.
+   try:
+       date = parse(old_d['date'])
+       date_info = dict(month=date.month, day=date.day, year=date.year)
+   except ValueError:
+       date = None 
+   if (date == None) or (date == parse("")):
        date_info = {}
    return merge(d, date_info,
                 dict(segment=seg, year=old_d['year'],
@@ -54,6 +61,8 @@ def row_for_segment(old_d, seg): # (str) -> str
 
 def main():
        fp = sys.argv[1]
-       run(fp)
+       segment_dir = sys.argv[2]
+       assert os.path.isdir(segment_dir)
+       run(fp, segment_dir)
 if __name__ == '__main__': main()       
        
